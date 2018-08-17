f491b5df2a687ad34930f51d1b1c8caa30dea7fc
kg <kgoss@me.com>
2018-08-17 10:38:51 -0700

    return user numer and reject negative numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,18 @@
 // business logic
 
-var ifItContainsZero = "Beep!";
-var ifItContainsOne = "Boop!";
-var ifItIsDivByThree= "I'm sorry, Dave. I'm afraid I can't do that.";
+var zero = "Beep!";
+var one = "Boop!";
+var divByThree= "I'm sorry, Dave. I'm afraid I can't do that.";
 var result = [];
+
+// var num = ('#userInput');
+// num.toString().indexOf('0')
+// return zero;
 
 var ifContainsZero = function() {
   beep.forEach(function(beep) {
-    if(number )
-  }
+
+    (userInput).indexOf('0')
 });
 }
 
@@ -17,9 +21,17 @@ $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
 
-var userInput = $('#userInput').val();
+    var number = parseInt($('#userInput').val());
+
+    if (number > 0) {
+      $("#errorDisplay").text("Please enter a positive number.");
+     console.log(number);
+   } else {
+     return number;
+   }
 
 
-  $("#result").show();
+  $("#errorDisplay").text("");
+  $("#result").text("");
   });
 });
