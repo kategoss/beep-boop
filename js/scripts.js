14a95452bd9f1da92958bdd3ba414e9649eeb5ad
kg <kgoss@me.com>
2018-08-17 09:22:52 -0700

    create content for HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 // business logic
 
+var ifItContainsZero = "Beep!";
+var ifItContainsOne = "Boop!";
+var ifItIsDivByThree= "I'm sorry, Dave. I'm afraid I can't do that.";
+var result = [];
+
+var ifContainsZero = function() {
+  beep.forEach(function(beep) {
+    if(number )
+  }
+});
+}
 
 // user interface logic
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
 
+var userInput = $('#userInput').val();
+
+
+  $("#result").show();
   });
 });
